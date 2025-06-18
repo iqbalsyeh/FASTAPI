a196416ab96a6a1a1c9da2c5af816c14b7550c91
iqbalsyeh <iqbalsyehmaulana1@gmail.com>
2025-06-18 11:34:28 +0700

    Fix Procfile and deploy again

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-import uvicorn
 
 app = FastAPI()
 
 @app.post("/cek-pdf")
 async def cek_pdf(file: UploadFile = File(...)):
-    # Baca nama file dan isi file (dummy saja)
     filename = file.filename
-
-    # Simulasi: misalnya kita bilang "dokumen ini ditemukan SPM dan SP2D"
     hasil = {
         "filename": filename,
         "keywords_found": {
@@ -18,9 +14,4 @@ async def cek_pdf(file: UploadFile = File(...)):
             "SPP": True
         }
     }
-
     return JSONResponse(content=hasil)
-
-# Untuk development lokal
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
